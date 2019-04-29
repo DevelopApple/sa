69fceac9c8300e6e19bc5283e4768ab9bbe16192
DevelopApple <35068989+DevelopApple@users.noreply.github.com>
2019-04-29 19:34:19 +0400

    Update Tweak.h

--- Tweak.h
+++ Tweak.h
@@ -1,33 +1,153 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <UIKit/UIControl.h>
 
-@interface FLHGradientLayer : CAGradientLayer
+// Hide Carrier Text in Status Bar
+%hook UIStatusBarServiceItemView
+- (id)_serviceContentsImage {
+	if ([prefs boolForKey: @"hidecarrier"]){
+		return nil;
+		}else{
+			return %orig;
+		}
+	}
+- (CGFloat)extraRightPadding {
+	if ([prefs boolForKey: @"hidecarrier"]){
+		return 0.0f;
+		}else{
+			return %orig;
+		}
+	}
+- (CGFloat)standardPadding {
+	if ([prefs boolForKey: @"hidecarrier"]){
+		return 2.0f;
+		}else{
+			return %orig;
+	}
+}
+%end
 
-@end
+%hook _UIStatusBarData
+- (void)setBackNavigationEntry:(id)arg1 {
+  if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+    return;
+  }else{
+    %orig;
+  }
+}
+%end
 
-@interface SBHUDView : UIView
+// iPhone X Status bar
+%hook UIStatusBar_Base
++ (BOOL)forceModern {
+	if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+		return [prefs boolForKey:@"statusBarX"];
+	}else{
+		return %orig;
+	}
+}
++ (Class)_statusBarImplementationClass {
+	if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+		return NSClassFromString(@"UIStatusBar_Modern");
+	}else{
+		return %orig;
+	}
+  //return NSClassFromString(@"UIStatusBar_Modern");
+}
 
-@property (assign,nonatomic) float progress;
-@property (nonatomic, retain) FLHGradientLayer *flhLayer;
-@property (nonatomic, retain) FLHGradientLayer *flhBackgroundLayer;
--(void)setProgress:(float)arg1 ;
--(float)flhRealProgress;
 
-@end
++ (Class)_implementationClass {
+  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"12.0")) {
+    if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+      return NSClassFromString(@"UIStatusBar_Modern");
+    }else if([prefs boolForKey:@"statusBarPad"]){
+      return NSClassFromString(@"UIStatusBar_Modern");
+    }else{
+      return %orig;
+    }
+  }else{
+    return %orig;
+  }
+}
++ (void)_setImplementationClass:(Class)arg1 {
+  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"12.0")) {
+    if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+    %orig(NSClassFromString(@"UIStatusBar_Modern"));
+    }else if([prefs boolForKey:@"statusBarPad"]){
+      %orig(NSClassFromString(@"UIStatusBar_Modern"));
+    }else{
+      %orig;
+    }
+  }else{
+    %orig;
+  }
+}
+%end
 
-@interface SBVolumeHUDView : SBHUDView
 
-@property(assign, nonatomic) int mode;
 
-@end
+%hook _UIStatusBar
+//TODO ONLY FOR SPLIT 58
++ (double)heightForOrientation:(long long)arg1 {
+  if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED ){
+    if (arg1 == 1 || arg1 == 2) {
+        return %orig - 10;
+    } else {
+        return %orig;
+    }
+  }else{
+    return %orig;
+  }
+    
+}
+%end 
 
-@interface SBRingerHUDView : SBHUDView
+%hook _UIStatusBarVisualProvider_iOS
++ (Class)class {
+  /*NSString *currentApp;
+  if (![CURRENT_BUNDLE isEqualToString:@"com.apple.springboard"]){
+	  SpringBoard *springBoard = (SpringBoard *)[UIApplication sharedApplication];
+	  SBApplication *frontApp = (SBApplication *)[springBoard _accessibilityFrontMostApplication];
+	  currentApp = [frontApp valueForKey:@"_bundleIdentifier"];
+  }else{
+    currentApp = CURRENT_BUNDLE;
+  }*/
+  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"12.0")) {
+    if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+      return NSClassFromString(@"_UIStatusBarVisualProvider_Split58");
+    }else if([prefs boolForKey:@"statusBarPad"]){
+      return NSClassFromString(@"_UIStatusBarVisualProvider_Pad_ForcedCellular");
+    }else if (@available(iOS 12.1, *)){
+      return NSClassFromString(@"_UIStatusBarVisualProvider_RoundedPad_ForcedCellular");
+    }else{
+      return NSClassFromString(@"_UIStatusBarVisualProvider_Pad_ForcedCellular");//%orig;
+    } 
+    //
+  }else{
+    return %orig;
+  }
+}
+%end
+// Thanks duraid
 
--(BOOL)isSilent;
+%hook UIStatusBarWindow
++ (void)setStatusBar:(Class)arg1 {
+  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"12.0")) {
+    if([prefs boolForKey:@"statusBarX"] && !IS_BLOCKED){
+      %orig(NSClassFromString(@"UIStatusBar_Modern"));
+    }else if([prefs boolForKey:@"statusBarPad"]){
+      %orig(NSClassFromString(@"UIStatusBar_Modern"));
+    }else{
+      %orig;
+    }
+  }else{
+    %orig;
+  }  
+}
+%end
 
-@end
-
-@interface SBHUDWindow : UIWindow
-
-@end
+%ctor {
+  if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"12.0")){
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if ([fileManager fileExistsAtPath:@"/var/mobile/Library/Preferences/HomeGesture/setup"]){
+      %init();
+    }
+  }
+}
